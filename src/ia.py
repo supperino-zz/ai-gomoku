df7a91ca7a2f594985f6ccd500f71efa0d0a669d
Lucas Suppes <lucas.suppes@gmail.com>
2018-09-26 01:48:50 -0300

    ia adicionada. faltando heuristica e o minimax

---
+++ src/ia.py
@@ -0,0 +1,19 @@
+
+class IA:
+    def __init__(self):
+        pass
+
+    def next_move(self, board):
+            print('next move')
+            return (0,0)
+
+    def _minimax(self):
+        pass
+
+    def _heuristics(self):
+        pass
+
+        """
+        Procurar por todas as jogadas possíveis,
+        Calcular a heuristica:= (triplas, quadr (da ia) - o mesmo do jogador)
+        """
